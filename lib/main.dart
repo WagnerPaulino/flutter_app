7a6d33207d99c7d83c68da0996ab35ac20a11f86
Wagner Paulino <wagnersantos9778@gmail.com>
2019-02-02 12:13:51 -0300

    Step 4 - first app flutter

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,30 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
+class RandomWordsState extends State<RandomWords> {
+@override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {//Criar uma classe de state
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class MyApp extends StatelessWidget {
+final title = 'Welcome to Flutter';
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(//linguagem que padroniza a web e o mobile
-      title: 'Welcome to Flutter',
       home: Scaffold(//Scaffot fornece um app bar padrão
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text(title),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
