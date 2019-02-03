73ed925ac9436e804c0f882375c2ea51a82d705a
Wagner Paulino <wagnersantos9778@gmail.com>
2019-02-03 17:44:50 -0300

    step 4 - first flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,39 @@ void main() => runApp(MyApp());
 
 class RandomWordsState extends State<RandomWords> {
 @override
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {//função chamada é chamada uma vez por palavra sugerida e coloca cada sugestão dentro da listView
+          if (i.isOdd) return Divider();//Adiciona uma linha de pixel antes de cada linha da lista
+
+          final index = i ~/ 2;//divide por dois e retorna um inteiro()
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));//Gera mais 10 palavras quando o usuario chega no fim do scroll
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Startup Name Generator'),
+        ),
+        body: _buildSuggestions(),
+      );
   }
 }
 
@@ -17,18 +47,12 @@ class RandomWords extends StatefulWidget {//Criar uma classe de state
 }
 
 class MyApp extends StatelessWidget {
-final title = 'Welcome to Flutter';
+final title = 'Startup Name Generator';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(//linguagem que padroniza a web e o mobile
-      home: Scaffold(//Scaffot fornece um app bar padrão
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+        title: title,
+        home: RandomWords(),
     );
   }
 }
