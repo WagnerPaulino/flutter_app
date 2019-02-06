664feb0b35b1407d1b99c6aa6d655d048933dcff
Wagner Paulino <wagnersantos9778@gmail.com>
2019-02-05 22:22:14 -0300

    FooterButton - incompleto

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,36 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class RandomWordsState extends State<RandomWords> {
-@override
+  @override
   final _suggestions = <WordPair>[];
   final Set<WordPair> _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions() {
+    //Metodo chamado no metodo build
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {//função chamada é chamada uma vez por palavra sugerida e coloca cada sugestão dentro da listView
-          if (i.isOdd) return Divider();//Adiciona uma linha de pixel antes de cada linha da lista
+        itemBuilder: (context, i) {
+          //função chamada é chamada uma vez por palavra sugerida e coloca cada sugestão dentro da listView
+          if (i.isOdd)
+            return Divider(); //Adiciona uma linha de pixel antes de cada linha da lista
 
-          final index = i ~/ 2;//divide por dois e retorna um inteiro()
+          final index = i ~/ 2; //divide por dois e retorna um inteiro()
           if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));//Gera mais 10 palavras quando o usuario chega no fim do scroll
+            _suggestions.addAll(generateWordPairs().take(
+                10)); //Gera mais 10 palavras quando o usuario chega no fim do scroll
           }
           return _buildRow(_suggestions[index]);
         });
   }
 
   void _pushSaved() {
+    //Metodo passado como parametro no IconButton
     Navigator.of(context).push(
-      new MaterialPageRoute<void>(   // Add 20 lines from here...
-        builder: (BuildContext context) {//builder retorna um Scaffold, contendo a nova app bar para a nova rota
+      new MaterialPageRoute<void>(
+        // Add 20 lines from here...
+        builder: (BuildContext context) {
+          //builder retorna um Scaffold, contendo a nova app bar para a nova rota
           final Iterable<ListTile> tiles = _saved.map(
             (WordPair pair) {
               return new ListTile(
@@ -37,13 +44,12 @@ class RandomWordsState extends State<RandomWords> {
               );
             },
           );
-          final List<Widget> divided = ListTile
-            .divideTiles(
-              context: context,
-              tiles: tiles,
-            )
-            .toList();
-          return new Scaffold(         // Add 6 lines from here...
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+          return new Scaffold(
+            // Add 6 lines from here...
             appBar: new AppBar(
               title: const Text('Saved Suggestions'),
             ),
@@ -54,7 +60,6 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
-
   Widget _buildRow(WordPair pair) {
     final bool alreadySaved = _saved.contains(pair);
     return ListTile(
@@ -63,11 +68,12 @@ class RandomWordsState extends State<RandomWords> {
         style: _biggerFont,
       ),
       trailing: new Icon(
-            alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved ? Colors.red : null,
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
       onTap: () {
-        setState(() {//setState dispara uma chamada para o metodo build resultando numa atualização da UI
+        setState(() {
+          //setState dispara uma chamada para o metodo build resultando numa atualização da UI
           if (alreadySaved) {
             _saved.remove(pair);
           } else {
@@ -80,30 +86,43 @@ class RandomWordsState extends State<RandomWords> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Startup Name Generator'),
-          actions: <Widget>[
-              new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
-          ],
-        ),
-        body: _buildSuggestions(),
-      );
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+      persistentFooterButtons: <Widget>[
+        BottomSheet(builder: (builder) {
+          return Container(height: 60.0, child: Text('I am text'));
+        
+        },
+        onClosing: _closeModal)
+      ],
+    );
   }
+
+  void _closeModal() {
+    print('modal closed');
+  }
+
 }
 
-class RandomWords extends StatefulWidget {//Criar uma classe de state
+class RandomWords extends StatefulWidget {
+  //Criar uma classe de state
   @override
   RandomWordsState createState() => new RandomWordsState();
 }
 
 class MyApp extends StatelessWidget {
-final title = 'Startup Name Generator';
+  final title = 'Startup Name Generator';
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(//linguagem que padroniza a web e o mobile
-        title: title,
-        home: RandomWords(),
+    return MaterialApp(
+      //linguagem que padroniza a web e o mobile
+      title: title,
+      home: RandomWords(),
     );
   }
 }
-
